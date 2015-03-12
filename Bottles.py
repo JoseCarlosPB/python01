a201ef8e50475ffced7dd61bb5fdaba0eb40c10d
josecarlospb <carlos.poncebaron@alum.uca.es>
2015-03-12 14:20:22 +0000

    Practica 1

---
+++ Bottles.py
@@ -0,0 +1,7 @@
+botella = 99
+while botella>0:
+    print botella, "bottles of beer on the wall, ", botella, " bottles of beer. " \
+    " Take one down, pass it around, ", botella-1, " bottles of beer on the wall."
+    botella = botella -1
+
+    
